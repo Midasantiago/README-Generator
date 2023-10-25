340e2da590745acf706ed1d9b848b5b510a67541
Bixey <mida.santiago98@gmail.com>
2023-10-25 00:14:31 -0400

    Added file writing function

--- Develop/index.js
+++ Develop/index.js
@@ -21,7 +21,7 @@ const questions = [
     {
         type: 'input',
         name: 'usage',
-        message: 'Provide instrcutions for use'
+        message: 'Provide instructions for use.'
     },
     {
         type: 'list',
@@ -80,11 +80,67 @@ const questions = [
     }
 ];
 
+let generateREADME = function(data) {
+    return `
+# ${data.title}
+    
+## Description
+    
+${data.description}
+    
+## Table of Contents
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+    
+## Installation
+    
+${data.installation}
+    
+## Usage
+    
+${data.usage}
+    
+## License
+    
+${data.license.name}
+    
+## Contributing
+    
+${data.contributing}
+    
+## Test
+    
+${data.test}
+    
+## Questions
+    
+My Github account is [GitHub.com/${data.githubUsername}](github.com/${data.githubUsername})
+    
+If you wish to contact me you can do so at ${data.email}
+    `
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+let writeToFile = function(content) {
+    fs.writeFile('README.md', content, (err) => {
+        err ? console.log(err) : console.log('Professional README Created!')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+let init = function() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            const READMEContent = generateREADME(data);
+            writeToFile(READMEContent);
+        })
+        .catch((error) => console.log(error));
+};
 
 // Function call to initialize app
 init();
